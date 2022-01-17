7f2447529b5a3b2cd096674ac3bd6abeb27b2e00
Manuel Liu <manuliu03@gmail.com>
2022-01-17 17:57:22 +0000

    login feito

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
-function login()
-{
-    let nome=document.getElementById("nome").value;
-    let texto="Logado como "+nome;
-    document.getElementById("teste").innerHTML=texto;
+function login() {
+    let nome = document.getElementById("nome").value;
+    let passw = document.getElementById("passw").value;
+    if(nome=="admin" && passw=="admin")
+    {
+        alert("Ola")
+    }
+    return false
 }
+
+
+function logSubmit(event) {
+    log.textContent = 'A sua subscrição foi feita com sucesso!';
+    event.preventDefault();
+}
+
+const form = document.getElementById('subs');
+const log = document.getElementById('log');
+form.addEventListener('submit', logSubmit);
+document.getElementById("log").style.fontSize = "25px"; 
+document.getElementById("log").style.color = "black";
+document.getElementById("log").style.backgroundColor = "orange"; 
