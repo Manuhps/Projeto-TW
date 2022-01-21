76452c676cdc00a52a859f4b87d25617d2e693e2
Manuel Liu <manuliu03@gmail.com>
2022-01-21 16:08:18 +0000

    feito

--- js/script.js
+++ js/script.js
@@ -37,3 +37,27 @@ document.getElementById("log").style.backgroundColor = "orange";
 
 
 
+function registar() {
+    let passw1 = document.getElementById("passw1").value;
+    let confpass = document.getElementById("confpass").value;
+    if(passw1==confpass)
+    {
+             
+        function logSubmit(event) {
+            registo.textContent = 'Registo efetuado com sucesso!';
+            event.preventDefault();
+        }
+        const registar = document.getElementById('registar');
+        const registo = document.getElementById('registo');
+        registar.addEventListener('submit', logSubmit);
+        document.getElementById("registo").style.fontSize = "25px"; 
+        document.getElementById("registo").style.color = "black";
+        document.getElementById("registo").style.backgroundColor = "orange"; 
+    }
+    else
+    {
+        alert("As suas passwords estao diferentes!")
+    }
+    return false
+
+}
